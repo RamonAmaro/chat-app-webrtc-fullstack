d0c836ea220248434f3e36d08146218d87fd9ee2
ramonamaro <ramonamaro.dev@gmail.com>
2024-10-23 14:15:40 -0300

    feat: implement meet with chat app with webrtc

---
+++ common/constants/index.ts
@@ -0,0 +1,16 @@
+import { ToastContainerProps } from 'react-toastify';
+
+export const ROOM_NAME = 'room';
+export const WINDOW_SIZE_IN_SAMPLES = 1024;
+export const MYSELF = 'You';
+export const SOCKET_PATH = '/api/socketio';
+export const TOAST_PROPS: ToastContainerProps = {
+  position: 'bottom-left',
+  theme: 'dark',
+  autoClose: 3000,
+};
+export const FAILURE_MSG =
+  'Opa!!! Não foi possível criar o stream para você. Tente novamente mais tarde 🫠';
+export const LOADER_STREAM_MSG =
+  'Carregando... Preparando sua transmissão de vídeo... 🚀';
+export const LOADER_PEER_MSG = 'Preparando... 🎮';
